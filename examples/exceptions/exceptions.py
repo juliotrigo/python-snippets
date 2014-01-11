d7ddfb8ef73430a677cef3a9f8c0347a8c406086
juliotrigo <juliotrigocom@gmail.com>
2014-01-11 23:37:15 +0000

    Exceptions pep8.

--- examples/exceptions/exceptions.py
+++ examples/exceptions/exceptions.py
@@ -2,17 +2,23 @@
 
 """Exceptions."""
 
+
 class MyException(Exception):
-    
+
     """General base exception."""
-    
+
     def __init__(self, value):
         """Assigns the text error."""
         self.value = value
-        
+
     def __str__(self):
         """Prints the error."""
         return str(self.value)
-    
+
+    def __repr__(self):
+        """Representation of the error."""
+        return str(self.value)
+
+
 class IncorrectParameters(MyException):
     """The parameters are incorrect."""
